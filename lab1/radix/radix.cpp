b567b39b4647562514153c5c94c8390936a8524a
Vadim <romanof.romanoff@yandex.ru>
2016-09-11 22:04:34 +0300

    Add files via upload

--- lab1/radix/radix.cpp
+++ lab1/radix/radix.cpp
@@ -1,37 +1,71 @@
 #include "stdafx.h"
 #include <iostream>
-#include <limits.h>
 
 using namespace std;
 
 static const string FILE_PATH = "../replace/file/";
 
+int ConvertCharToInt(char const& ch)
+{
+	int digit = -1;
+	if ((ch >= '0') && (ch <= '9'))
+	{
+		digit = ch - 0x30; // Преобразование символа в число 
+	}
+	else if ((ch >= 'A') && (ch <= 'Z'))
+	{
+		digit = int(ch) - int('A') + 10;
+	} 
+	return digit;
+}
+
 int StringToInt(const string& str, int radix, bool & wasError)
 {
-	return 1;
+	int number = 0;
+	int digit = 0;
+	int maxIntDiv = INT_MAX / 10;
+	int maxIntMod = INT_MAX % 10;
+	for (int i = 0; i != str.length(); i++)
+	{
+		digit = ConvertCharToInt(str[i]);
+		if (digit != -1)
+		{
+			cout << digit;
+		}
+	}
+	return number;
 }
 
 void TranslationOfRadix(int & source, int & destination, std::string const& value)
 {
-	bool wasError;
+	bool wasError = false;
 	StringToInt(value, source, wasError);
 
 }
 
+bool PresenceOfEmptyData(char *argv[])
+{
+	return (argv[1] == "") && (argv[2] == "") && (argv[3] == "");
+}
+
 int main(int argc, char *argv[])
 {
-	std::cout << INT_MAX << std::endl;
+	//std::cout << INT_MAX << std::endl;
 	//int a = 2147483647;
 	//cout << a << endl;
 	/*if (argc != 4)
 	{
 		cout << "Invalid number of parameters!\n";
 		return 1;
-	}*/
-	/*int source = atoi(argv[1]);
+	}
+	int source = atoi(argv[1]);
 	int destination = atoi(argv[2]);
-	std::string value = argv[3];*/
-
+	std::string value = argv[3];
+	if (PresenceOfEmptyData(argv))
+	{
+		cout << "There are empty data!!!" ;
+		return 1;
+	}*/
 	int source = atoi("16");
 	int destination = atoi("10");
 	std::string value = "1F";
