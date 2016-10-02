1c8178419b93d9bfcc6f2023fb532f974c5bfade
Vadim <romanof.romanoff@yandex.ru>
2016-10-03 00:54:14 +0300

    Add files via upload

--- lab1/binrle/rle_test.bat
+++ lab1/binrle/rle_test.bat
@@ -4,29 +4,27 @@ set PROGRAM="%~1"
 if ERRORLEVEL 1 goto err
 
 %PROGRAM% unpack output\out1PACK.txt output\out1UNPACK.txt
-fc input\in1.txt output\out1UNPACK.txt
+fc /b input\in1.txt output\out1UNPACK.txt
+if ERRORLEVEL 1 goto err
+
+
+rem character with code 255
+%PROGRAM% pack input\in2.txt output\out2PACK.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% unpack output\out2PACK.txt output\out2UNPACK.txt
+fc /b input\in2.txt output\out2UNPACK.txt
 if ERRORLEVEL 1 goto err
 
 
 
 rem 255 identical characters
-%PROGRAM% pack input\in2.txt output\out2PACK.txt
-if ERRORLEVEL 1 goto err
-
-%PROGRAM% unpack output\out2PACK.txt output\out2UNPACK.txt
-fc input\in2.txt output\out2UNPACK.txt
-if ERRORLEVEL 1 goto err
-
-
-
-rem empty file
 %PROGRAM% pack input\in3.txt output\out3PACK.txt
 if ERRORLEVEL 1 goto err
 
 %PROGRAM% unpack output\out3PACK.txt output\out3UNPACK.txt
-fc input\in3.txt output\out3UNPACK.txt
+fc /b input\in3.txt output\out3UNPACK.txt
 if ERRORLEVEL 1 goto err
-
 
 
 rem 256 identical characters
@@ -34,7 +32,7 @@ rem 256 identical characters
 if ERRORLEVEL 1 goto err
 
 %PROGRAM% unpack output\out4PACK.txt output\out4UNPACK.txt
-fc input\in4.txt output\out4UNPACK.txt
+fc /b input\in4.txt output\out4UNPACK.txt
 if ERRORLEVEL 1 goto err
 
 
